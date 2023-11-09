8de9c1fe8c4967ef009ed88ad72749c46280a05f
edwin <edwinkun@gmail.com>
2023-11-09 14:04:57 +0700

    adding route :pig2:

---
+++ src/main/java/com/edw/route/TimerRoute.java
@@ -0,0 +1,22 @@
+package com.edw.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.timer;
+
+
+/**
+ * <pre>
+ *     com.edw.route.TimerRoute
+ * </pre>
+ *
+ * @author Muhammad Edwin < edwin at redhat dot com >
+ * 09 Nov 2023 13:58
+ */
+public class TimerRoute  extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from(timer("foo").period(3000))
+                .log("Hello Dunia");
+    }
+}
