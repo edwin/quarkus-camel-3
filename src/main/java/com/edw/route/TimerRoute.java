e845f0b2039cdfca3a9454865cb41c5f2eff4797
edwin <edwinkun@gmail.com>
2023-11-09 14:07:09 +0700

    change wording :frog:

--- src/main/java/com/edw/route/TimerRoute.java
+++ src/main/java/com/edw/route/TimerRoute.java
@@ -16,7 +16,7 @@ public class TimerRoute  extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from(timer("foo").period(3000))
-                .log("Hello Dunia");
+        from(timer("foo").period(1000))
+                .log("Hello World");
     }
 }
